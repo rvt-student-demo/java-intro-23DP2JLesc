82f439de0cf087043937ed9c21035164ab53d7ef
23DP2JLesc <166109641+23DP2JLesc@users.noreply.github.com>
2024-09-11 09:33:28 +0000

    Uzdevums 1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,19 +1,23 @@
 package lv.rvt;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        int a = 3;
-        double b = 5.5;
-        String c = "None!";
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Chicken : " + a);
-        System.out.println("Bacon (kg): " + b);
-        System.out.println("Tractor: " + c);
-        System.out.println("And finally, a summary: " + a+ ' ' + b + ' ' + c);
+        System.out.println("Ievadi savu vārd");
+        String name = scanner.nextLine();
+
+        System.out.println("Ievadi savu uzvārdu");
+        String surname = scanner.nextLine();
+
+        System.out.println("Ievadi savu grupu");
+        String group = scanner.nextLine();
+
+        System.out.println(name + ' ' + surname + ' ' + group);
 
     }
 
