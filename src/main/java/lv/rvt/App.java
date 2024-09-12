11413870206185e6517f4fe6bfba545ba4fe4810
23DP2JLesc <166109641+23DP2JLesc@users.noreply.github.com>
2024-09-12 08:32:48 +0000

    Addition formula

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,18 +6,17 @@ public class App
 {
     public static void main( String[] args )
     {
+        
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ievadi savu vārd");
-        String name = scanner.nextLine();
+        System.out.println("Ievadi pirmo skaitli");
+        int firstnumber = Integer.valueOf(scanner.nextLine());
 
-        System.out.println("Ievadi savu uzvārdu");
-        String surname = scanner.nextLine();
+        System.out.println("Ievadi otro skaitli");
+        int secondnumber = Integer.valueOf(scanner.nextLine());
 
-        System.out.println("Ievadi savu grupu");
-        String group = scanner.nextLine();
-
-        System.out.println(name + ' ' + surname + ' ' + group);
+        int result = firstnumber + secondnumber;
+        System.out.println(firstnumber + " + " + secondnumber + " = " +result);
 
     }
 
