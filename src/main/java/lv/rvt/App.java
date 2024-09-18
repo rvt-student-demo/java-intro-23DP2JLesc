4d995ddd7b78b66744e06b0c2bc19d6c8bb647dd
23DP2JLesc <166109641+23DP2JLesc@users.noreply.github.com>
2024-09-18 08:22:45 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,27 +9,32 @@ public class App
         
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ievadi pirmo skaitli");
-        int firstnumber = Integer.valueOf(scanner.nextLine());
-
-        System.out.println("Ievadi otro skaitli");
-        int secondnumber = Integer.valueOf(scanner.nextLine());
-
-        int result = firstnumber + secondnumber;
-
-        int result1 = firstnumber - secondnumber;
-
-        int result2 = firstnumber * secondnumber;
-
-        float result3 = firstnumber / secondnumber;
-
-        System.out.println(firstnumber + " + " + secondnumber + " = " +result);
-
-        System.out.println(firstnumber + " - " + secondnumber + " = " +result1);
-
-        System.out.println(firstnumber + " * " + secondnumber + " = " +result2);
-
-        System.out.println(firstnumber + " / " + secondnumber + " = " +result3);
+        System.out.println("Ievadi savus puktus 0-100");
+        int mark = Integer.valueOf(scanner.nextLine());
+        if (mark < 0) {
+            System.out.println("Grade: Inpossible!");
+        }
+        else if(mark <= 49){
+            System.out.println("Grade: failed");
+        }
+        else if(mark <= 59){
+            System.out.println("Grade: 1");
+        }
+        else if(mark <= 69){
+            System.out.println("Grade: 2");
+        }
+        else if(mark <= 79){
+            System.out.println("Grade: 3");
+        }
+        else if(mark <= 89){
+            System.out.println("Grade: 4");
+        }
+        else if(mark <= 100){
+            System.out.println("Grade: 5");
+        }
+        else if(mark > 100){
+            System.out.println("Grade: incredible!");
+        }
 
     }
 
