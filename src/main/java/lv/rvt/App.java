07becde0577aee82cd45778d405bcea9518ded40
23DP2JLesc <166109641+23DP2JLesc@users.noreply.github.com>
2024-10-09 09:15:10 +0000

    Star sign (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,18 +4,18 @@ import java.util.*;
 
 import java.util.Scanner;
 
-public class App
-{
-    public static void main(String[] args) {
-        printUntilNumber(5, 20);
+
+
+public class App{
+
+
+
+    public static void main(String[] args) {       
+
+        Stars.printStars(5);
+        Stars.printStars(3);
+        Stars.printStars(9);
        
     }
-    public static void printUntilNumber(int first, int second) {
-        while(first != second){
-            if (first%3 == 0){
-                System.out.println(first);
-            }
-            first++;
-        }
+    
     }
-}
---
+++ src/main/java/lv/rvt/Stars.java
@@ -0,0 +1,10 @@
+package lv.rvt;
+
+public class Stars {
+    public  static void printStars(int stars){
+        for(int i = 0; i < stars; i++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
