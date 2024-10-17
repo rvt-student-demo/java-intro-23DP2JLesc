4c021fdca0a35cddd42cdc535cdd2bc30284834e
23DP2JLesc <166109641+23DP2JLesc@users.noreply.github.com>
2024-10-17 09:35:25 +0000

    Exercise 3B — Revised Guessing Game

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,26 +8,30 @@ public class App{
         Scanner scanner = new Scanner(System.in);
 
         int rand1 = rand.nextInt(10);
-
+        int c = 5;
         System.out.println("I am thinking of a number from 1 to 10.");
         System.out.println("You must guess what it is in three tries.");
         System.out.println("Enter a guess:");
+        System.out.println("Your beginning score is "+ c +" points.");
         while(true){
         int guess = Integer.valueOf(scanner.nextLine());
-        if (guess != rand1 && (rand1-guess >= 3 && guess-rand1  <= 3) ){
-            System.out.println("Wrong");
-            System.out.println("cold");
-        }else if(guess != rand1 && (rand1-guess >= 2 && guess-rand1  <= 2) ){
-            System.out.println("Wrong");
-            System.out.println("warm");
-        }
-        else if(guess != rand1 && (rand1-guess >= 1 && guess-rand1  <= 1) ){
-            System.out.println("Wrong");
-            System.out.println("hot");
-        }
+        if (guess != rand1 && rand1 > guess ){
+            c = c-1;
+            System.out.println("Too low! Your score is now " + c);
+            System.out.println("Enter a guess:");
+        }else if(guess != rand1 && rand1 < guess ){
+            c = c-1;
+            System.out.println("Too high! Your score is now " + c);
+            System.out.println("Enter a guess:");
+        }   
         else{
-            System.out.println("Righ!");
+            System.out.println("You WON the game!");
+            System.out.println("Your score is" + c);
             break;
+        }
+        if(c == 0){
+            System.out.println("You lost.");
+            System.out.println("Your score is " + c);
         }
         }
     }
