ce003282bed92f66651b7f046a2b37ba3cefc221
23DP2JLesc <166109641+23DP2JLesc@users.noreply.github.com>
2024-11-07 08:22:28 +0000

    Agent

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,11 +3,15 @@ package lv.rvt;
 import java.util.*;
 import lv.rvt.Person;
 import lv.rvt.Product;
+import lv.rvt.DecreasingCounter;
+import lv.rvt.Agent;
 
 public class App{
-    public static void main(String[] args) {  
-        Product ada = new Product("Bannana", 2.5, 10);
+    public static void main(String[] args) {
+        Agent bond = new Agent("James", "Bond");
+        System.out.println(bond); 
 
-        ada.printProduct();
+        Agent ionic = new Agent("Ionic", "Bond");
+        System.out.println(ionic); 
     }
 }
---
+++ src/main/java/lv/rvt/DecreasingCounter.java
@@ -0,0 +1,21 @@
+package lv.rvt;
+
+public class DecreasingCounter {
+    private int value; 
+    
+    public DecreasingCounter(int initialValue) {
+        this.value = initialValue;
+    }
+    
+    public void printValue() {
+        System.out.println("value: " + this.value);
+    }
+    
+    public void decrement() {
+            // write the method implementation here
+            // the aim is to decrement the value of the counter by one
+    }
+    
+        // and the other methods go here
+}
+
